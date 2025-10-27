616fa18915ae1ce1df9ac1a293018e9f69af8400
Joost Lubach <joostlubach@gmail.com>
2025-10-27 11:50:42 +0100

    Again fix lint

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,37 +1,44 @@
 const FS = require('fs')
 const YAML = require('js-yaml')
 
-module.exports = {
-  languageOptions: {
-    globals: {
-      es6: true,
-      browser: false,
+module.exports = [
+  // Global ignores (applies to all configs)
+  {
+    ignores: [
+      'bin/**',
+      'dist/**',
+      'node_modules/**',
+      'tsconfig.json'
+    ]
+  },
+  
+  {
+    files: [
+      'src/**/*.ts',
+    ],
+
+    languageOptions: {
+      globals: {
+        ...require('globals').node,
+        ...require('globals').es2022,
+      },
+      
+      parser: require('@typescript-eslint/parser'),
+      parserOptions: {
+        project: 'tsconfig.json',
+        tsconfigRootDir: __dirname,
+        sourceType: 'module',
+        ecmaVersion: 2022,
+      }
     },
-    
-    parser: require('@typescript-eslint/parser'),
-    parserOptions: {
-      project: 'tsconfig.json',
-      tsconfigRootDir: __dirname,
-      sourceType: 'module',
-    }
-  },
 
-  plugins: {
-    '@stylistic': require('@stylistic/eslint-plugin'),
-    'unused-imports': require('eslint-plugin-unused-imports'),
-    '@mosdev': require('@mosdev/eslint-plugin')
-  },
+    plugins: {
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+      '@stylistic': require('@stylistic/eslint-plugin'),
+      'unused-imports': require('eslint-plugin-unused-imports'),
+      '@mosdev': require('@mosdev/eslint-plugin')
+    },
 
-  files: [
-    'src/**/*.ts',
-  ],
-
-  ignores: [
-    'bin',
-    'dist',
-    'node_modules',
-    'tsconfig.json'
-  ],
-
-  rules: YAML.load(FS.readFileSync('../../eslint.rules.yml'))
-};
+    rules: YAML.load(FS.readFileSync('../../eslint.rules.yml'))
+  }
+]
